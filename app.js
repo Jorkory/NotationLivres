d1818de4832bd8240b04933f30dddd48e0645d01
Joris Lamaque <jor57lam@gmail.com>
2023-12-05 15:29:34 +0100

    Ajout: mongoose + les schemas mongoose

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 const express = require('express');
+const mongoose = require('mongoose')
 
 const app = express();
+
+mongoose.connect('mongodb+srv://jorkory:MLafu6qeMmWUcPdq@atlascluster.z7gp1ht.mongodb.net/?retryWrites=true&w=majority',
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(() => console.log('Connexion à MongoDB réussie !'))
+    .catch(() => console.log('Connexion à MongoDB échoué !'));
 
 app.use((req, res) => {
     res.json({ message: 'Serveur connecté !' })
