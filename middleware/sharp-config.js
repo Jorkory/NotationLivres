d2bf9a1c7d95ca2b9e67c959052e596af33dc7db
Joris Lamaque <jor57lam@gmail.com>
2023-12-12 10:05:27 +0100

    Fixbug: créer l'erreur pour fichier non conforme

--- middleware/sharp-config.js
+++ middleware/sharp-config.js
@@ -4,15 +4,20 @@ module.exports = (req, res, next) => {
     if (req.file) {
         req.filename = req.file.originalname.replace(/ /g, '_').split('.')[0] + Date.now() + '.webp';
 
-        sharp(req.file.buffer)
-            .resize({
-                width: 404,
-                height: 568,
-                fit: sharp.fit.cover,
-            })
-            .toFile(`./images/` + req.filename)
-            .then(() => next())
-            .catch(error => res.status(500).json({ error }));
+        try {
+            sharp(req.file.buffer)
+                .resize({
+                    width: 404,
+                    height: 568,
+                    fit: sharp.fit.cover,
+                })
+                .toFile(`./images/` + req.filename)
+                .then(() => next())
+                .catch(error => res.status(500).json({ error }));
+        } catch {
+            const err = new Error("Le fichier téléchargé n'est pas conforme.");
+            res.status(400).json({ err });
+        }
     } else {
         next();
     }
